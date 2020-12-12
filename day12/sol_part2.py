6be38f34940f751bdfb45f5ab11f5403a1d8b738
Lucas Antchougov <lucasangov@hotmail.com>
2020-12-12 22:29:48 +0100

    change solution for part 2 of day 12: only store relative position of waypoint to ship instead of real position

--- day12/sol_part2.py
+++ day12/sol_part2.py
@@ -1,22 +1,17 @@
 from math import sin, cos, radians
 
 
-def rotate_point(point, pivot_point, angle):
+def rotate_point(x, y, angle):
     """
-    Rotate a point about a pivot point by a given angle.
+    Rotate a point about the origin by a given angle.
     """
-    x, y = point
-    x_pivot, y_pivot = pivot_point
 
     angle = radians(angle)
     s = int(sin(angle))
     c = int(cos(angle))
 
-    x -= x_pivot
-    y -= y_pivot
-
-    x_new = x * c - y * s + x_pivot
-    y_new = x * s + y * c + y_pivot
+    x_new = x * c - y * s
+    y_new = x * s + y * c
 
     return x_new, y_new
 
@@ -24,14 +19,14 @@ def rotate_point(point, pivot_point, angle):
 with open("input.txt") as f:
 
     x_ship, y_ship = 0, 0
-    x_wayp, y_wayp = 10, 1
+    x_wayp, y_wayp = 10, 1  # coordinates of waypoint relative to ship
 
     for ins in f:
         ins = ins.strip()
         ins_type = ins[0]
         ins_val = int(ins[1:])
         if ins_type in ("N", "S", "E", "W"):
-            # instruction is to move waypoint in given direction
+            # Instruction is to move waypoint in given direction.
             if ins_type == "N" or ins_type == "S":
                 dy = ins_val if ins_type == "N" else -ins_val
                 y_wayp += dy
@@ -39,19 +34,14 @@ with open("input.txt") as f:
                 dx = ins_val if ins_type == "E" else -ins_val
                 x_wayp += dx
         elif ins_type in ("L", "R"):
-            # instruction is to rotate waypoint about the ship by given angle
+            # Instruction is to rotate waypoint about the ship by given angle.
+            # Because we keep the coordinates of the waypoint relative to the ship, we
+            # pretend we are rotating the waypoint about the origin (0, 0).
             angle = ins_val if ins_type == "L" else -ins_val
-            x_wayp, y_wayp = rotate_point((x_wayp, y_wayp), (x_ship, y_ship), angle)
+            x_wayp, y_wayp = rotate_point(x_wayp, y_wayp, angle)
         else:
-            # instruction is to move ship towards waypoint
-            # first we move ship towards waypoint
-            x_diff = x_wayp - x_ship
-            y_diff = y_wayp - y_ship
-            x_ship += x_diff * ins_val
-            y_ship += y_diff * ins_val
-
-            # then we also move waypoint so that its position relative to ship stays the same
-            x_wayp = x_ship + x_diff
-            y_wayp = y_ship + y_diff
+            # Instruction is to move ship towards waypoint.
+            x_ship += x_wayp * ins_val
+            y_ship += y_wayp * ins_val
 
     print(abs(x_ship) + abs(y_ship))
